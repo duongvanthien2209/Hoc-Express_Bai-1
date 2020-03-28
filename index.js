64bdc2243a7d3575addc979ff632570e336218a7
Duong Van Thien <duongvanthienbkhoa@gmail.com>
2020-03-28 20:26:32 +0700

    Cai dat template engine

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('views', './views'); // Set folder cho các view
+app.set('view engine', 'pug'); // Set view engine là pug
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', name: 'Thien', list: [{name: 'Thien', age: 24}, {name: 'Thao', age: 24},] })
+});
+
 app.get('/index', (req, res) => res.send('<h1>Hello World!</h1>'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
