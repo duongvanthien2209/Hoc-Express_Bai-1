daa23108c7a6dec46dad571c854aa429a56f29ac
Duong Van Thien <duongvanthienbkhoa@gmail.com>
2020-03-28 21:43:57 +0700

    install nodemon

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.set('views', './views'); // Set folder cho các view
 app.set('view engine', 'pug'); // Set view engine là pug
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 const data = [
     {id: 1, name: 'Thien', age: 24},
@@ -11,15 +16,29 @@ const data = [
     {id: 3, name: 'Huy', age: 22}
 ];
 
-app.get('/', function (req, res) {
-    res.render('index', { title: 'List', list: data });
+app.get('/index', function (req, res) {
+    res.render('list', { list: data });
+});
+
+app.get('/getSearch', (req,res) => {
+    res.render('index');
 });
 
 app.get('/search', (req,res) => {
     let name = req.query.name;
-    res.render('index', { title: 'List', list: data.filter(item => item.name.indexOf(name) !== -1) })
+    res.render('list', { list: data.filter(item => item.name.indexOf(name) !== -1) });
 });
 
-app.get('/index', (req, res) => res.send('<h1>Hello World!</h1>'));
+app.get('/create', (req,res) => {
+    res.render('create');
+});
+
+app.post('/create', (req,res) => {
+    let { name, age } = req.body;
+    data.push({name, age: parseInt(age) });
+    res.redirect('/index');
+});
+
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
