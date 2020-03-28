b591e64184dea0e1277f613872d9fc13ad42929a
Duong Van Thien <duongvanthienbkhoa@gmail.com>
2020-03-28 20:47:43 +0700

    Query Parameter

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const port = 3000;
 app.set('views', './views'); // Set folder cho các view
 app.set('view engine', 'pug'); // Set view engine là pug
 
+const data = [
+    {id: 1, name: 'Thien', age: 24},
+    {id: 2, name: 'Thao', age: 24},
+    {id: 3, name: 'Huy', age: 22}
+];
+
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', name: 'Thien', list: [{name: 'Thien', age: 24}, {name: 'Thao', age: 24},] })
+    res.render('index', { title: 'List', list: data });
+});
+
+app.get('/search', (req,res) => {
+    let name = req.query.name;
+    res.render('index', { title: 'List', list: data.filter(item => item.name.indexOf(name) !== -1) })
 });
 
 app.get('/index', (req, res) => res.send('<h1>Hello World!</h1>'));
